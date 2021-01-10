5703e0f7ad511d28b93dbaeea2bb159ac0b5ab8c
Mateusz Wicki <71140843+Wicki07@users.noreply.github.com>
2021-01-10 17:55:05 +0100

    Lab10

---
+++ Lab10/backend/todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Todo(models.Model):
+  title = models.CharField(max_length=120)
+  description = models.TextField()
+  completed = models.BooleanField(default=False)
+
+  def _str_(self):
+    return self.title
+    assert errors[0].msg.startswith("CORS_ORIGIN_ALLOW_ALL should be") # to należy dodać
+    
